8b19ccdff610cb845adae865a70825780bfb6e88
Adroaldo <hygino@msn.com>
2025-04-02 11:40:07 -0300

    Insert and Update Hero

--- src/main/java/br/dev/hygino/heroes/controller/HeroController.java
+++ src/main/java/br/dev/hygino/heroes/controller/HeroController.java
@@ -1,13 +1,18 @@
 package br.dev.hygino.heroes.controller;
 
+import br.dev.hygino.heroes.dto.RequestHeroDTO;
 import br.dev.hygino.heroes.dto.ResponseHeroDTO;
 import br.dev.hygino.heroes.service.HeroService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +40,22 @@ public class HeroController {
             return ResponseEntity.status(HttpStatus.OK).body(res);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseHeroDTO> insertHero(@RequestBody @Valid RequestHeroDTO dto) {
+        ResponseHeroDTO res = service.insertHero(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateHero(@PathVariable Long id, @RequestBody @Valid RequestHeroDTO dto) {
+        try {
+            ResponseHeroDTO res = service.updateHero(id, dto);
+            return ResponseEntity.status(HttpStatus.OK).body(res);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 }
--- src/main/java/br/dev/hygino/heroes/service/HeroService.java
+++ src/main/java/br/dev/hygino/heroes/service/HeroService.java
@@ -1,8 +1,10 @@
 package br.dev.hygino.heroes.service;
 
+import br.dev.hygino.heroes.dto.RequestHeroDTO;
 import br.dev.hygino.heroes.dto.ResponseHeroDTO;
 import br.dev.hygino.heroes.model.Hero;
 import br.dev.hygino.heroes.repository.HeroRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -29,5 +31,35 @@ public class HeroService {
                 .orElseThrow(() -> new IllegalArgumentException("Não existe heroi com o id: " + id));
 
         return new ResponseHeroDTO(res);
+    }
+
+    @Transactional
+    public ResponseHeroDTO insertHero(RequestHeroDTO dto) {
+        Hero entity = new Hero();
+        dtoToEntity(dto, entity);
+        entity = repository.save(entity);
+
+        return new ResponseHeroDTO(entity);
+    }
+
+    private void dtoToEntity(RequestHeroDTO dto, Hero entity) {
+        entity.setName(dto.name());
+        entity.setCodename(dto.codename());
+        entity.setAge(dto.age());
+        entity.setCountry(dto.country());
+        entity.setTeam(dto.team());
+    }
+
+    @Transactional
+    public ResponseHeroDTO updateHero(Long id, RequestHeroDTO dto) {
+        try {
+            Hero entity = repository.getReferenceById(id);
+            dtoToEntity(dto, entity);
+            entity = repository.save(entity);
+
+            return new ResponseHeroDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new IllegalArgumentException("Não existe heroi com o id: " + id);
+        }
     }
 }
